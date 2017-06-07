76d7669c798d5caac5a50a54fcfe92c72d69ee3f
wanls4583 <409051987@qq.com>
2017-06-07 18:05:03 +0800

    随机出现雪糕筒

--- src/Game.js
+++ src/Game.js
@@ -1,13 +1,15 @@
 (function(){
     Laya.init(640,400);
-    Laya.stage.scaleMode = Laya.Stage.SCALE_NOBORDER
+    Laya.stage.scaleMode = Laya.Stage.SCALE_EXACTFIT
     var bg = new Background();
     Laya.stage.addChild(bg);
     var AniConfPath = "res/atlas/race.json";
     Laya.loader.load(AniConfPath, Laya.Handler.create(this, createTong), null, Laya.Loader.ATLAS);
     
     function createTong(){
-        var tong = new Tong();
-        Laya.stage.addChild(tong);
+        Laya.timer.frameLoop(30,this,function(){
+            var tong = new Tong();
+            Laya.stage.addChild(tong);
+        });
     }
 })()
--- src/Tong.js
+++ src/Tong.js
@@ -3,9 +3,14 @@ var Tong = (function(_super){
         Tong.super(this);
         this.createFrames();
         this.play(Util.TONG);
-        this.scale(0.2,0.2);
+        this.scale(0,0);
         this.come();
     }
+    //雪糕筒初始y坐标
+    Tong.beginY = 30;
+    Tong.maxScale = 1;
+    //雪糕筒最大速率基数，基数越大，速度变化越快
+    Tong.maxSpeedRatioBase = 40;
     Laya.class(Tong,'Tong',_super);
     Tong.prototype.createFrames = function(){
         if(!Tong.hasCached){
@@ -20,16 +25,36 @@ var Tong = (function(_super){
         }
         this.ani.play(0,true,action);
         var bounds = this.ani.getGraphicBounds();
-        this.ani.pos(-bounds.width/2,-bounds.height/2);
+        this.ani.pos(-bounds.width/2,-bounds.height);
     }
     Tong.prototype.come = function(){
+        /**计算speedRatioBase的加速度(物理公式:1/2*a*t^2=s)，
+         * s代表speedRatioBase，t代表y偏移量，计算出加速度a，
+         * 雪糕筒越近speedRatioBase越大
+         * */
+        var acceleration = 2*Tong.maxSpeedRatioBase/Math.pow(Laya.stage.height-Tong.beginY,2);
         var rnd = Math.ceil(Math.random()*3);
-        var x,y=30,toX,toY;
-        x = Laya.stage.width / 2 - 15 + rnd * 10;
-        toX = (Laya.stage.width - 40 * 2) / 3 * rnd + 40;
-        toY = Laya.stage.height;
-        this.pos(x,y);
-        Laya.Tween.to(this,{x:toX,y:toY},3000);
+        var beginX,beginY = Tong.beginY,toX,toY = Laya.stage.height;
+        //x轴随机位置
+        beginX = 308 + (rnd-1) * 10;
+        //运动到眼前时的x轴位置
+        toX = (Laya.stage.width - 80 * 2) / 3 * (rnd-1) + 140;
+        this.pos(beginX,beginY);
+        var ratio = (toX-beginX)/(toY-beginY); 
+        //雪糕筒移动
+        Laya.timer.frameLoop(1,this,function(){
+            var speedRatio = 1/2*acceleration*Math.pow(this.y+1-Tong.beginY,2);
+            this.y+=1*(1+speedRatio);
+            this.x+=ratio*(1+speedRatio);
+            if(this.y < toY){
+                var scale = Tong.maxScale*this.y/toY;
+                this.scale(scale,scale);
+            }
+            //消失时销毁对象
+            if(this.y > Laya.stage.height+this.ani.getGraphicBounds().height){
+                this.destroy();
+            }
+        });
     }
     return Tong;
 })(Laya.Sprite)
